1e2c7cd8c1107b0a8ffa3b2669ec072f50d9772b
Paweł Kubiak <pawel.kubiak@protonmail.com>
2023-02-09 12:46:34 +0100

    Initial version (#1)
    * feat: Initial script version
    
    * feat: Get rid of jquery dependency
    
    * feat: improved package quality
    
    * fix: updated project link
    
    * doc: updated doc and script link
    
    * docs: Add github ribbon to example.html
    
    * fix: script domain
    
    * doc: updated README.md
    
    * doc: Add more compatibility checks
    
    * doc: Describe available options
    
    * fix: Normalized quotes in renderer.html

--- README.md
+++ README.md
@@ -1,1 +1,128 @@
 # nbembed.js
+
+Self-contained JavaScript script for embedding Jupyter Notebooks into websites.
+
+<!--## Features:
+- -->
+
+## ⛑&#xFE0F; USAGE
+
+1. Include recent version of the script into your website:
+    ```html
+    <script src="https://pawelkubiak.me/nbembed.js/dist/nbembed.js"></script>
+    ```
+    or download it and use your local copy.
+
+2. Insert `notebook` tag in place where you would like to render your notebook. Place url to your notebook raw `.ipynb` file as a `src` attribute:
+    ```html
+    <notebook src="<notebook-url>"></notebook>
+    ```
+    Check common usage scenarios at the end of the section for more info.
+    
+
+3. (Optional) You can adjust rendering properties using `config` attribute:
+    ```html
+    <notebook src="..." config="<config-options>"></notebook>
+    ```
+    Allowed options are described in the the [next section](#-options).  Multiple options can be provided by separating them using `,` (comma).
+
+
+<details>
+    <summary><b>Scenario: Embedding Notebook from GitHub:</b></summary> 
+    <p>To use a notebook hosted on the GitHub platform, you must use its raw-link. To get it, open the notebook in the GitHub file browser, then click on the "Raw" or "Download" button visible on the right side above the Notebook preview.
+    You should see the Notebook's source code, copy and use the link to this page.<br>
+    <img src="" /><br>Your obtained link should look like: <code>https://raw.githubusercontent.com/{username}/{repo}/{branch}/{file}</code>.</p>
+    <p>For example, to embed file:<br><code>https://github.com/empet/Math/blob/master/DomainColoring.ipynb</code><br>
+    You should use url:<br><code>https://raw.githubusercontent.com/empet/Math/master/DomainColoring.ipynb</code>
+    </p>
+</details>
+<br>
+
+<details>
+    <summary><b>Scenario: Embedding on Confluence page:</b></summary>
+    TBA
+</details>
+
+## 🖼&#xFE0F; MINIMAL EXAMPLE
+
+Below you can check minimal code demonstrating `nbembed.js` usage:
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://pawelkubiak.me/nbembed.js/dist/nbembed.js"></script>
+</head>
+
+<body>
+    <notebook 
+        src="https://raw.githubusercontent.com/empet/Math/master/DomainColoring.ipynb"
+        config="hide-prompts"
+        style="max-width: 1000px; margin:0 auto"></notebook>
+</body>
+</html>
+```
+
+Live version can be previewed here: https://pawelkubiak.me/nbembed.js/docs/example.html
+
+
+## 🔧 OPTIONS
+
+| option | description |
+|--------|-------------|
+| `hide-prompts` | Hide cells numbering (ie. `In[..]`, `Out[..]`) |
+| `hide-stdout` | Hide cells standard output (eg. text displayed with `print(..)`) |
+| `hide-stderr` | Hide cells errors output |
+| `hide-code` | Hide cells source code |
+| `hide-markdown` | Hide markdown cells |
+
+
+## 🕵️‍♀️ SECURITY
+Package use `Iframe Sandboxing` technique to separate notebook content from outer website.
+This makes it possible to execute scripts (like interactive plots) nested in the notebook without security risks.
+
+
+## 💻 COMPATIBILITY
+
+<table>
+<tr><th>Browser</th><th>Tested Versions</th></tr>
+<tr><td colspan="2"><b>Desktop</b></td></tr>
+<tr><td>Google Chrome</td><td>✔&#xfe0f; 109.0.5414.120</td></tr>
+<tr><td>Microsoft Edge</td><td>✔&#xfe0f; 80.0 and newer</td></tr>
+<tr><td>Mozilla Firefox</td><td>✔&#xfe0f; 107.0</td></tr>
+<tr><td>Safari on MacOS</td><td>✔&#xfe0f; 16</td></tr>
+<tr><td>Internet Explorer</td><td>❌ 11</td></tr>
+
+<tr><td colspan="2"><b>Mobile</b></td></tr>
+<tr><td>Chrome for Android</td><td>✔&#xfe0f; 108.0.5195.136</td></tr>
+<tr><td>Safari for iOS</td><td>✔&#xfe0f; 16.0</td></tr>
+<tr><td>Samsung Internet</td><td>✔&#xfe0f; 17.0.9.34</td></tr>
+<tr><td>Mozilla Firefox</td><td>✔&#xfe0f; 108.1.1</td></tr>
+</table>
+
+
+<!-- 
+## TODO:
+- [ ] Click image to zoom
+- [ ] Themes support
+- [ ] Add link to original file (download link?)
+- [ ] Display load time (somewhere)
+- [ ] Displaying multiple notebooks
+- [ ] Better Errors Handling
+    - [ ] 404 Error
+    - [ ] CORS Error
+    - [ ] Wrong format
+- [ ] Create Google Action for building javascript source
+- [ ] Fix Latex Rendering Problems
+- [ ] Create external TOC
+- [] z jakimi prawami dostepu odpala sie kod wewnątrz sendbox'a? czy ma prawa usera? czy anonimowe?
+- [ ] Collapsible sections
+- [ ] Update renderer script dependencies
+- [ ] Allow disabling JavaScript
+- [ ] Add example with different options
+- [ ] Allow dynamically creating notebooks
+- [ ] Move renderer styles to repo
+- [ ] Describe nbembed.js features in README.md
+-->
---
+++ dist/nbembed.js
@@ -0,0 +1,77 @@
+const RENDERER_HTML = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"/><title>Jupyter Notebook Renderer</title><script src="https://code.jquery.com/jquery-1.7.2.min.js"></script><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.12.0/katex.min.css"/><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism.min.css"/><link rel="stylesheet" href="https://jsvine.github.io/nbpreview/css/notebook.css"/><script>function onload() { window.parent.postMessage({ type: "ready" }, "*"); } function updateHeight() { let body = document.body, html = document.documentElement; let height = Math.max(body.scrollHeight, body.offsetHeight, html.offsetHeight); window.parent.postMessage({ type: "resize", height: height }, "*"); } const resizeObserver = new ResizeObserver(updateHeight); resizeObserver.observe(document.querySelector("html")); function showError(details) { let alert = document.querySelector("div.alert"); alert.classList.add("error"); alert.innerText = "Exception occured during Jupyter Notebook loading!"; if (details) { let el = document.createElement("small"); el.innerText = details; alert.appendChild(el); } } function configureNotebook(config) { if (config.font) { document.body.style.fontSize = config.font; } for (let key of ["hide-stdout", "hide-stderr", "hide-code", "hide-markdown", "hide-prompts"]) { if (config[key] === true) document.body.classList.add(key); } } function renderNotebook(content, config) { let el = document.body; console.log("Rendering Notebook with config: ", config); configureNotebook(config); try { var notebook = nb.parse(JSON.parse(content)); } catch { return showError("Wrong notebook format"); } var rendered = notebook.render(); el.innerHTML = ""; el.appendChild(rendered); Prism.highlightAll();$(el).find(".nb-html-output script").each((i, script) => { $(script).replaceWith($(script).clone()); }); updateHeight(); } function format_progress(event) { let humanize = (count) => ((count / 1000 / 1000).toFixed(2) + "MB"); if (event.total !== null) return humanize(event.loaded) + " / " + humanize(event.total); return "~ " + humanize(event.loaded); } window.addEventListener("message", function (event) { if (event.source === window.parent) { let data = event.data; if (data.type === "render") renderNotebook(data.data, data.config || {}); if (data.type === "progress") { document.querySelector("#progress-indicator label").innerText = format_progress(data); if (data.total) { document.querySelector("#progress-indicator progress").max = data.total; document.querySelector("#progress-indicator progress").value = data.loaded; } } if (data.type === "error") showError(); } });</script><style>body{font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size: 16px;}.nb-notebook{padding-left: 100px;}img{max-width: 100%;}.nb-output{overflow-x: auto !important;}.nb-cell{clear:both;}/* Hide parts of notebook */body.hide-prompts .nb-notebook{padding-left: 0px;}body.hide-stdout .nb-text-output,body.hide-stderr .nb-stderr,body.hide-code .nb-input,body.hide-markdown .nb-markdown-cell,body.hide-prompts .nb-output::before, body.hide-prompts .nb-input::before{display: none;}/* Default style for notebook tabels */.nb-html-output table{border: none; border-collapse: collapse; border-spacing: 0; color: black; table-layout: fixed;}.nb-html-output thead{border-bottom: 1px solid black; vertical-align: bottom;}.nb-html-output tbody tr:nth-child(odd){background: #f5f5f5;}.nb-html-output tbody tr:hover{background: rgba(66, 165, 245, 0.2);}.nb-html-output tr, .nb-html-output th, .nb-html-output td{text-align: right; vertical-align: middle; padding: 0.5em 0.5em; line-height: normal; white-space: normal; max-width: none; border: none;}/* Format progressbar */#progress-indicator{font-size: 12px;margin-top: 10px;}#progress-indicator progress{margin-right: 10px;}/* Alerts */.alert{color: #084298;background-color: #cfe2ff;border-color: #b6d4fe;font-size: 20px;padding: 20px;text-align: center;border: 1px solid transparent;border-radius: .25rem;}.alert small{display: block;margin-top: 10px;}.error{color: #842029;background-color: #f8d7da;border-color: #f5c2c7;}</style></head><body onload="onload()"><div class="alert">Please Wait! Jupyter Notebook is loading!<div id="progress-indicator"><progress></progress><label></label></div></div><script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.14/es5-shim.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/marked/2.0.0/marked.min.js"></script><script src="https://cdn.jsdelivr.net/npm/ansi_up@1.1.3/ansi_up.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/prism.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/components/prism-markup.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/components/prism-python.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.12.0/katex.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.12.0/contrib/auto-render.min.js"></script><script src="https://jsvine.github.io/nbpreview/js/vendor/notebook.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js" integrity="sha512-c3Nl8+7g4LMSTdrm621y7kf9v3SDPnhxLNhcjFJbKECVnmZHTdo+IRO05sNLTH/D3vA6u1X32ehoLC7WFVdheg==" crossorigin="anonymous"></script></body></html>';
+const PROJECT_LINK = '<div style="text-align:right;font-size:12px;opacity:0.6;padding-right:20px">Rendered with <a href="https://github.com/pkubiak/nbembed.js" target="_blank">nbembed.js</a></div>';
+
+
+function loadJupyter(url, iframe, config) {
+	let send = (data) => {iframe.contentWindow.postMessage(data, '*')};
+	
+    const xhr = new window.XMLHttpRequest();
+
+    // NOTE: to disable mime type auto guessing
+    xhr.overrideMimeType("text/plain; charset=utf-8");
+    xhr.responseType = "text";
+
+    xhr.addEventListener("progress", function(e){
+        send({
+            type: 'progress',
+            loaded: e.loaded,
+            total: e.lengthComputable ? e.total : null
+        });
+    });
+
+    xhr.addEventListener("load", (e) => {
+        send({type: 'render', data: xhr.response, config: config})
+    });
+    xhr.addEventListener("error", () => {
+        send({type: 'error'});
+    });
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function buildIframe() {
+	let iframe = document.createElement('iframe');
+	iframe.srcdoc = RENDERER_HTML;
+	iframe.sandbox.add('allow-scripts');
+	iframe.scrolling = "no";
+	iframe.style.border = '0';
+	iframe.style.width = '100%';
+	return iframe;
+}
+
+function parseConfig(configStr) {
+	let config = {};
+	for(let key of configStr.split(','))
+		if(key) {
+			let [k, v] = key.split(':');
+			config[k] = v || true;
+		}
+	return config;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	document.querySelectorAll("notebook").forEach(function(el) {
+		let url = el.getAttribute('src');
+		let config = parseConfig(el.getAttribute('config') || "");
+		
+		// Build renderer iframe
+		let iframe = buildIframe();
+		el.style.display = 'block';
+		el.appendChild(iframe);
+		
+		el.insertAdjacentHTML('beforeend', PROJECT_LINK);
+		
+		// Message dispatching
+		window.addEventListener("message", (event) => {
+			if(event.source === iframe.contentWindow) {
+				let data = event.data;
+
+				if(data.type === 'resize')
+					iframe.style.height = Math.ceil(data.height) + 'px';
+				if(data.type === 'ready')
+					loadJupyter(url, iframe, config);
+			};
+		});	
+	});
+});
